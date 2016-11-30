85444843d600c0dc9dec6d6ea88566382b41e131
Anny He <anny.he@salesforce.com>
2016-11-30 09:33:54 -0800

    initial test commit

---
+++ tests/api/v1/samples/filter.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2016, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
+
+/**
+ * tests/api/v1/subjects/getHierarchyAspectAndTagsFilters.js
+ */
+'use strict';
+
+const supertest = require('supertest');
+const api = supertest(require('../../../../index').app);
+const constants = require('../../../../api/v1/constants');
+const tu = require('../../../testUtils');
+const u = require('./utils');
+const Subject = tu.db.Subject;
+const Aspect = tu.db.Aspect;
+const path = '/v1/subjects/{key}/hierarchy';
+const expect = require('chai').expect;
+
+// create aspect, subject, samples
